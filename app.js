d99e04d9363a229d99a12eca48524edde8cf8bbc
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-18 04:04:25 +0900

    classList를 통해 불러온 것의 class 배열을 바꿀 수 있음

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const hellos =document.querySelector(".hello h1");
+const h1 =document.querySelector("div.hello:first-child h1");
 //querySelector : css방식으로 1개의element를 불러올 수 있음 , querySelectorAll: 조건에 맞는 모든 element를 불러옴 (array 형태로)
 function handleTitleClick(){
-    const currentColor = hellos.style.color;
-    let newColor;
-    if(currentColor ==="blue"){
-        newColor = "tomato";
+    const clickedClass = "clicked"
+    if(h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
     }else{
-        newColor="blue";
+        h1.classList.add(clickedClass);
     }
-    hellos.style.color=newColor;
 }
 
 
-hellos.addEventListener("click", handleTitleClick);
-//자바 스크립트가 실행버튼을 누르기 때문에 handleTitleClick를 사용,
-//handleTitleClick()은 내가 실행하는 것이기 때문
+h1.addEventListener("click", handleTitleClick);
+
