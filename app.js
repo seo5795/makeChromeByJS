c1e0cef95953ee460b325641256c2906796638ca
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-18 04:07:46 +0900

    toggle class

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const h1 =document.querySelector("div.hello:first-child h1");
 //querySelector : css방식으로 1개의element를 불러올 수 있음 , querySelectorAll: 조건에 맞는 모든 element를 불러옴 (array 형태로)
-function handleTitleClick(){
-    const clickedClass = "clicked"
-    if(h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    }else{
-        h1.classList.add(clickedClass);
-    }
+function handleTitleClick(){ 
+    h1.classList.toggle("clicked");
 }
-
+//const clickedClass = "clicked";
+// if(h1.classList.contains(clickedClass)){
+//     h1.classList.remove(clickedClass);
+// }else{
+//     h1.classList.add(clickedClass);
+// }와 같은 역할
 
 h1.addEventListener("click", handleTitleClick);
 
