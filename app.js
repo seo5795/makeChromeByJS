7cd4e6cabfc0b8d35361e98646f71727a5b5e0ea
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-18 10:44:15 +0900

    no message

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ const h1 =document.querySelector("div.hello:first-child h1");
 function handleTitleClick(){ 
     h1.classList.toggle("clicked");
 }
-//const clickedClass = "clicked";
-// if(h1.classList.contains(clickedClass)){
-//     h1.classList.remove(clickedClass);
-// }else{
-//     h1.classList.add(clickedClass);
-// }와 같은 역할
+
 
 h1.addEventListener("click", handleTitleClick);
 
