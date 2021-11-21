7d7d048d986754df2a6231148bcbd24cb0a1ed6d
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-22 03:38:34 +0900

    form태그 이용하여 js에서 value 받기

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const h1 =document.querySelector("div.hello:first-child h1");
-//querySelector : css방식으로 1개의element를 불러올 수 있음 , querySelectorAll: 조건에 맞는 모든 element를 불러옴 (array 형태로)
-function handleTitleClick(){ 
-    h1.classList.toggle("clicked");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function BtnClick(){
+  const username = loginInput.value;
+  console.log(username);
 }
 
-
-h1.addEventListener("click", handleTitleClick);
-
+loginButton.addEventListener("click",BtnClick);
