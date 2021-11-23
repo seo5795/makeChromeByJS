89dbfd24cfa894fe8436f657839fd13c40e815fb
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-24 02:19:34 +0900

    4.6 Loading Username: 이름 정보를 localStorage에 저장하여 존재여부에 따라 보여주는 hidden을 지워줌

--- app.js
+++ app.js
@@ -4,21 +4,31 @@ const greeting = document.querySelector("#greeting");
 //const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY="username";
 
 function onLoginSubmit(event){
   event.preventDefault();
   //브라우저에서 default로 하려는 행동을 막음
   const username = loginInput.value;
-  localStorage.setItem("username",username);
+  localStorage.setItem(USERNAME_KEY,username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
+}
+
+function paintGreetings(username){
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+  paintGreetings(savedUsername);
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
+}
 
 // onLoginSubmit({일어난 이벤트에 대한 정보를 객체로 전달함})
 
