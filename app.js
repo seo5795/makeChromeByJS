acfbfaa7cc257ed2a8ff8afd7624f191ed2989f0
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-23 12:38:01 +0900

    4.4Getting Username

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+//const link = document.querySelector("a");
 
-function BtnClick(){
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event){
+  event.preventDefault();
   const username = loginInput.value;
-  console.log(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click",BtnClick);
+// function handleLinkClick(event){
+//     event.preventDefault();
+//     //브라우저에서 하려는 행동을 막음
+//     console.log(event);
+//     alert("clicked");
+// }
+
+loginForm.addEventListener("submit", onLoginSubmit);
+//link.addEventListener("click", handleLinkClick);
+
+// handleLinkClick({일어난 이벤트에 대한 정보를 객체로 전달함})
+
