f993d012adb93550f14d2eead7096342d4730b60
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-23 12:46:16 +0900

    3.5 saving username-localStorage: 미니 db와 같이 정보를 저장하는 역할

--- app.js
+++ app.js
@@ -7,21 +7,18 @@ const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
   event.preventDefault();
+  //브라우저에서 default로 하려는 행동을 막음
   const username = loginInput.value;
+  localStorage.setItem("username",username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// function handleLinkClick(event){
-//     event.preventDefault();
-//     //브라우저에서 하려는 행동을 막음
-//     console.log(event);
-//     alert("clicked");
-// }
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
-//link.addEventListener("click", handleLinkClick);
 
-// handleLinkClick({일어난 이벤트에 대한 정보를 객체로 전달함})
+
+// onLoginSubmit({일어난 이벤트에 대한 정보를 객체로 전달함})
 
